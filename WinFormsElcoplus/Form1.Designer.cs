682601f87504dfc7af9cadd2be21b974b739bae2
Stupin97 <stupin.il@yandex.ru>
2020-10-06 16:41:58 +0700

    Тестовое задание

---
+++ WinFormsElcoplus/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WinFormsElcoplus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.loadXML = new System.Windows.Forms.Button();
+            this.boxURLs = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(21, 80);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(478, 358);
+            this.treeView.TabIndex = 0;
+            // 
+            // loadXML
+            // 
+            this.loadXML.Location = new System.Drawing.Point(403, 19);
+            this.loadXML.Name = "loadXML";
+            this.loadXML.Size = new System.Drawing.Size(75, 23);
+            this.loadXML.TabIndex = 1;
+            this.loadXML.Text = "Load XML";
+            this.loadXML.UseVisualStyleBackColor = true;
+            this.loadXML.Click += new System.EventHandler(this.loadXML_Click);
+            // 
+            // boxURLs
+            // 
+            this.boxURLs.FormattingEnabled = true;
+            this.boxURLs.Location = new System.Drawing.Point(11, 19);
+            this.boxURLs.Name = "boxURLs";
+            this.boxURLs.Size = new System.Drawing.Size(376, 21);
+            this.boxURLs.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.boxURLs);
+            this.groupBox1.Controls.Add(this.loadXML);
+            this.groupBox1.Location = new System.Drawing.Point(21, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(484, 57);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 449);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.treeView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button loadXML;
+        private System.Windows.Forms.ComboBox boxURLs;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WinFormsElcoplus/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WinFormsElcoplus
+{
+    public partial class Form1 : Form
+    {
+        public List<string> listURLs;
+        XmlDocument xmlDocument;
+        public Form1()
+        {
+            InitializeComponent();
+            CreateURLs();
+        }
+
+        void CreateURLs()
+        {
+            listURLs = new List<string>();
+            listURLs = ConfigurationManager.AppSettings["URLs"].Split(',').ToList();
+
+            foreach (var url in listURLs)
+                boxURLs.Items.Add(url);
+
+            if (boxURLs.Items.Count != 0)
+                boxURLs.SelectedIndex = 0;
+        }
+
+        private void loadXML_Click(object sender, EventArgs e)
+        {
+            xmlDocument = new XmlDocument();
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    xmlDocument.LoadXml(client.DownloadString(boxURLs.SelectedItem.ToString()));
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            treeView.Nodes.Clear();
+
+            //Корень
+            treeView.Nodes.Add(new TreeNode(xmlDocument.DocumentElement.Name));
+
+            TreeNode treeNode = new TreeNode();
+            treeNode = treeView.Nodes[0];
+
+            AddNodeFromXml(xmlDocument.DocumentElement , treeNode);
+        }
+
+        void AddNodeFromXml(XmlNode inXmlNode, TreeNode inTreeNode)
+        {
+            XmlNode xmlNode;
+            TreeNode treeNode;
+            XmlNodeList xmlNodeList;
+
+            //Если не достигли листа (еще имеет узлы)
+            if (inXmlNode.HasChildNodes)
+            {
+                xmlNodeList = inXmlNode.ChildNodes;
+                int ii = inXmlNode.ChildNodes.Count;
+
+                for (int i = 0; i < xmlNodeList.Count; i++)
+                {
+                    xmlNode = inXmlNode.ChildNodes[i];
+                    inTreeNode.Nodes.Add(new TreeNode(xmlNode.Name));
+                    treeNode = inTreeNode.Nodes[i];
+                    AddNodeFromXml(xmlNode, treeNode);
+                }
+            }
+            else
+            {
+                inTreeNode.Text = (inXmlNode.OuterXml).Trim();
+            }
+        }
+    }
+}
